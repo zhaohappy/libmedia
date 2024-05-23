b4fd380191b736f26b855dd0dcbbf0d15248e3c3
zhaohappy <2281756061@qq.com>
2024-05-23 22:24:36 +0800

    feat: intread & intwrite

--- src/avutil/util/intread.ts
+++ src/avutil/util/intread.ts
@@ -5,33 +5,33 @@ export function r8(p: pointer<void>) {
 }
 
 export function rl16(p: pointer<void>) {
-  return (r8(p) << 8) | r8(reinterpret_cast<pointer<uint8>>(p) + 1)
+  return (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) | r8(p)
 }
 
 export function rb16(p: pointer<void>) {
-  return (r8(reinterpret_cast<pointer<uint8>>(p) + 1) << 8) | r8(p)
+  return (r8(p) << 8) | r8(reinterpret_cast<pointer<uint8>>(p + 1))
 }
 
 export function rl24(p: pointer<void>) {
-  return (r8(p) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p) + 1) << 8) | r8(reinterpret_cast<pointer<uint8>>(p) + 2)
+  return (r8(reinterpret_cast<pointer<uint8>>(p + 2)) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) + r8(p)
 }
 
 export function rb24(p: pointer<void>) {
-  return (r8(reinterpret_cast<pointer<uint8>>(p) + 2) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p) + 1) << 8) + r8(p)
+  return (r8(p) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) | r8(reinterpret_cast<pointer<uint8>>(p + 2))
 }
 
 export function rl32(p: pointer<void>) {
-  return (rl16(p) << 16) | rl16(reinterpret_cast<pointer<uint8>>(p) + 2)
+  return (rl16(reinterpret_cast<pointer<uint8>>(p + 2)) << 16) | rl16(p)
 }
 
 export function rb32(p: pointer<void>) {
-  return (rb16(reinterpret_cast<pointer<uint8>>(p) + 2) << 16) | rb16(p)
+  return (rb16(p) << 16) | rb16(reinterpret_cast<pointer<uint8>>(p + 2))
 }
 
 export function rl64(p: pointer<void>) {
-  return (BigInt(rl32(p)) << 32n) | BigInt(rl32(reinterpret_cast<pointer<uint8>>(p) + 4))
+  return (BigInt(rl32(reinterpret_cast<pointer<uint8>>(p + 4))) << 32n) | BigInt(rl32(p))
 }
 
 export function rb64(p: pointer<void>) {
-  return (BigInt(rb32(reinterpret_cast<pointer<uint8>>(p) + 4)) << 32n) | BigInt(rb32(p))
+  return (BigInt(rb32(p)) << 32n) | BigInt(rb32(reinterpret_cast<pointer<uint8>>(p + 4)))
 }
