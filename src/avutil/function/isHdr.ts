4403bc20491fb9f05f0a200779496bc745139a96
zhaohappy <2281756061@qq.com>
2024-08-03 21:10:20 +0800

    feat: add isHdr function

---
+++ src/avutil/function/isHdr.ts
@@ -0,0 +1,18 @@
+import { PixelFormatDescriptorsMap } from '../pixelFormatDescriptor'
+import { AVMediaType } from '../codec'
+import AVCodecParameters from '../struct/avcodecparameters'
+import { AVColorTransferCharacteristic } from '../pixfmt'
+
+export function isHdr(parameters: pointer<AVCodecParameters>) {
+  if (parameters.codecType !== AVMediaType.AVMEDIA_TYPE_VIDEO) {
+    return false
+  }
+
+  const descriptor = PixelFormatDescriptorsMap[parameters.format]
+  if (!descriptor || descriptor.comp[0].depth <= 8) {
+    return false
+  }
+
+  return parameters.colorTrc === AVColorTransferCharacteristic.AVCOL_TRC_ARIB_STD_B67
+    || parameters.colorTrc === AVColorTransferCharacteristic.AVCOL_TRC_SMPTE2084
+}
