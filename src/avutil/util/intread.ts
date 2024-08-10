95fc82b1f8b4faf23bff1bb771f00a58a6dcf946
zhaohappy <2281756061@qq.com>
2024-08-10 16:21:50 +0800

    fix: 类型修正

--- src/avutil/util/intread.ts
+++ src/avutil/util/intread.ts
@@ -23,38 +23,38 @@
  *
  */
 
-export function r8(p: pointer<void>) {
+export function r8(p: pointer<void>): uint8 {
   return accessof(reinterpret_cast<pointer<uint8>>(p))
 }
 
-export function rl16(p: pointer<void>) {
+export function rl16(p: pointer<void>): uint16 {
   return (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) | r8(p)
 }
 
-export function rb16(p: pointer<void>) {
+export function rb16(p: pointer<void>): uint16 {
   return (r8(p) << 8) | r8(reinterpret_cast<pointer<uint8>>(p + 1))
 }
 
-export function rl24(p: pointer<void>) {
+export function rl24(p: pointer<void>): int32 {
   return (r8(reinterpret_cast<pointer<uint8>>(p + 2)) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) + r8(p)
 }
 
-export function rb24(p: pointer<void>) {
+export function rb24(p: pointer<void>): int32 {
   return (r8(p) << 16) | (r8(reinterpret_cast<pointer<uint8>>(p + 1)) << 8) | r8(reinterpret_cast<pointer<uint8>>(p + 2))
 }
 
-export function rl32(p: pointer<void>) {
+export function rl32(p: pointer<void>): int32 {
   return (rl16(reinterpret_cast<pointer<uint8>>(p + 2)) << 16) | rl16(p)
 }
 
-export function rb32(p: pointer<void>) {
+export function rb32(p: pointer<void>): int32 {
   return (rb16(p) << 16) | rb16(reinterpret_cast<pointer<uint8>>(p + 2))
 }
 
-export function rl64(p: pointer<void>) {
+export function rl64(p: pointer<void>): int64 {
   return (BigInt(rl32(reinterpret_cast<pointer<uint8>>(p + 4))) << 32n) | BigInt(rl32(p))
 }
 
-export function rb64(p: pointer<void>) {
+export function rb64(p: pointer<void>): int64 {
   return (BigInt(rb32(p)) << 32n) | BigInt(rb32(reinterpret_cast<pointer<uint8>>(p + 4)))
 }
