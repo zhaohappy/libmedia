445559c3fce24f5a184772e15f86b04a059a5a52
zhaohappy <2281756061@qq.com>
2024-09-25 21:07:52 +0800

    feat: demux 失败返回 DATA_INVALID 错误码

--- src/avformat/formats/IWavFormat.ts
+++ src/avformat/formats/IWavFormat.ts
@@ -188,7 +188,8 @@ export default class IWavFormat extends IFormat {
     }
     catch (error) {
       if (formatContext.ioReader.error !== IOError.END) {
-        logger.error(error.message)
+        logger.error(`read packet error, ${error}`)
+        return errorType.DATA_INVALID
       }
       return formatContext.ioReader.error
     }
