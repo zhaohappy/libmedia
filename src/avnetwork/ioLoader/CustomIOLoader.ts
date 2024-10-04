2c9033badd802b0fe7d94660fc1dce8d4f47beeb
zhaohappy <2281756061@qq.com>
2024-10-04 15:52:28 +0800

    refactor: CustomIOLoader 不继承 IOLoader，单独实现

--- src/avnetwork/ioLoader/CustomIOLoader.ts
+++ src/avnetwork/ioLoader/CustomIOLoader.ts
@@ -23,12 +23,12 @@
  *
  */
 
-import IOLoader from './IOLoader'
+import { Uint8ArrayInterface } from "common/io/interface"
+import { Data } from "common/types/type"
 
-export default abstract class CustomIOLoader extends IOLoader {
+export default abstract class CustomIOLoader {
 
   constructor() {
-    super()
   }
 
   /**
@@ -55,9 +55,44 @@ export default abstract class CustomIOLoader extends IOLoader {
   }
 
   /**
-   * 打开自定义 ioloader
+   * 打开 ioloader
+   * 
+   * @param info 
+   * @param range 
    * 
    * @returns 成功返回 0, 失败返回错误码（负值）
    */
   public abstract open(): Promise<int32>
+
+  /**
+   * 读取数据到缓冲区
+   * 
+   * @param buffer 可以放置数据的缓冲区，类 Uint8Array 结构
+   * @param options 一些配置（比如 hls 和 dash 有相关配置项）
+   * 
+   * @returns 返回写入的数据长度，失败返回错误码（负值）
+   */
+  public abstract read(buffer: Uint8ArrayInterface, options?: Data): Promise<int32>
+
+  /**
+   * seek 到指定位置
+   * 
+   * @param pos 位置
+   * @param options 一些配置（比如 hls 和 dash 有相关配置项）
+   * 
+   * @returns 成功返回 0, 否则失败，可以返回错误码（负值）
+   */
+  public abstract seek(pos: int64, options?: Data): Promise<int32>
+
+  /**
+   * 数据总字节大小
+   * 
+   * 没有返回 0n
+   */
+  public abstract size(): Promise<int64>
+
+  /**
+   * 停止 ioloader
+   */
+  public abstract stop(): Promise<void>
 }
