4b7097e25aafd5778ecf800d33fac32d8b5c2ec5
zhaohappy <2281756061@qq.com>
2024-10-12 20:18:15 +0800

    fix: dts parseHeader 修正

--- src/avformat/codecs/dts.ts
+++ src/avformat/codecs/dts.ts
@@ -25,20 +25,23 @@
 
 import BitReader from 'common/io/BitReader'
 import { Uint8ArrayInterface } from 'common/io/interface'
+import align from 'common/math/align'
 
 const DTSChannelTab = [1, 2, 2, 2, 2, 3, 3, 4, 4, 5, 6, 6, 6, 7, 8, 8]
 
-const DTSSampleRateTab = [0, 8000, 16000, 32000, 0, 0, 11025, 22050, 44100, 0, 0, 12000, 24000, 48000, 0, 0]
+const DTSSampleRateTab = [0, 8000, 16000, 32000, 0, 0, 11025, 22050, 44100, 0, 0, 12000, 24000, 48000, 96000, 192000]
 
 const DTSBitrateTab = [32000, 56000, 64000, 96000, 112000, 128000, 192000, 224000, 256000, 320000, 384000, 448000,
   512000, 576000, 640000, 768000, 960000, 1024000, 1152000, 1280000, 1344000, 1408000, 1411200, 1472000, 1536000,
   1920000, 2048000, 3072000, 3840000, 0, 0, 0
 ]
 
+export const DTS_PCMBLOCK_SAMPLES = 32
+
 export interface DTSHeaderInfo {
   syncWord: uint32
   frameType: int32
-  sampleCount: int32
+  deficitSamples: int32
   crcFlag: int32
   sampleBlock: int32
   frameSize: int32
@@ -52,13 +55,13 @@ export interface DTSHeaderInfo {
 }
 
 export function parseHeader(buf: Uint8ArrayInterface) {
-  const bitReader = new BitReader(size)
+  const bitReader = new BitReader(buf.length)
   bitReader.appendBuffer(buf)
 
   const info: DTSHeaderInfo = {
     syncWord: 0,
     frameType: 0,
-    sampleCount: 0,
+    deficitSamples: 0,
     crcFlag: 0,
     sampleBlock: 0,
     frameSize: 0,
@@ -73,15 +76,15 @@ export function parseHeader(buf: Uint8ArrayInterface) {
 
   info.syncWord = bitReader.readU(32)
 
-  if (info.syncWord !== 0x7ffe8001) {
+  if (info.syncWord !== 0x7ffe8001 && info.syncWord !== 0xfe7f0180) {
     return -1
   }
 
   info.frameType = bitReader.readU1()
-  info.sampleCount = bitReader.readU(5)
+  info.deficitSamples = bitReader.readU(5) + 1
   info.crcFlag = bitReader.readU1()
-  info.sampleBlock = bitReader.readU(7)
-  info.frameSize = bitReader.readU(14)
+  info.sampleBlock = bitReader.readU(7) + 1
+  info.frameSize = align(bitReader.readU(14) + 1, 4)
   info.channelIndex = bitReader.readU(6)
   info.sampleRateIndex = bitReader.readU(4)
   info.bitrateIndex = bitReader.readU(5)
