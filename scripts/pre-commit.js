00b556a9c85ad10f2659ca5105af8f628f97bfa3
zhaohappy <2281756061@qq.com>
2024-12-13 20:10:56 +0800

    chore: 新增 scripts 目录，添加 git hook

---
+++ scripts/pre-commit.js
@@ -0,0 +1,25 @@
+const { execSync, spawnSync } = require('child_process');
+
+const modifiedList = execSync('git diff --cached --name-only')
+  .toString()
+  .trim()
+  .split('\n')
+  .filter(file => {
+    return /^src\/\S*\.[j|t]s$/.test(file)
+  })
+
+const result = spawnSync(
+  'node',
+  [
+    './node_modules/eslint/bin/eslint.js',
+    ...modifiedList,
+    '-c=./eslint/typescript.js',
+    '--rulesdir=./eslint/rules',
+    '--ignore-path=./.eslintignore'
+  ],
+  {
+    stdio: 'inherit'
+  }
+)
+
+process.exit(result.status)
