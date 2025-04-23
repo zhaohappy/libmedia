22e03de0ef5a99868acc048f254e75c8730662dd
zhaohappy <2281756061@qq.com>
2025-04-23 22:34:28 +0800

    docs: IOFlags 添加注释解释

--- src/avutil/avformat.ts
+++ src/avutil/avformat.ts
@@ -74,8 +74,20 @@ export const enum IOType {
 
 export const enum IOFlags {
   NONE = 0,
+  /**
+   * 源可进行 seek 操作
+   */
   SEEKABLE = 1,
+  /**
+   * 源是切片类型的，如 hls 和 dash
+   */
   SLICE = 2,
+  /**
+   * 源来自于网络
+   */
   NETWORK = 4,
+  /**
+   * 源正在被 abort 标志
+   */
   ABORT = 8
 }
