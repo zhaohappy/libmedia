2c4bad5cb037202f9452514a9f2886ed9fe63020
zhaohappy <2281756061@qq.com>
2025-07-02 17:18:58 +0800

    feat(avcodec): 新增加一些解码器，用以支持 avi、rm、rmvb、asf、wma、wmv 等封装格式文件的播放
    1. AD PCM 族解码器
    2. dvaudio、dvvideo 解码器
    3. h261、h263 解码器
    4. msmpeg4 解码器
    5. real audio(cook、sipr、ralf) 解码器
    6. real video(rv10、rv20、rv30、rv40) 解码器
    7. wma、wmv 解码器

--- build/build-ffmpeg-decoder.sh
+++ build/build-ffmpeg-decoder.sh
@@ -118,6 +118,138 @@ if [[ $decode == "vp9" ]]; then
   fi
 fi
 
+if [[ $decode == "h263" ]]; then
+  realDecoder="h263"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=h263i --enable-decoder=h263p"
+fi
+
+if [[ $decode == "msmpeg4" ]]; then
+  realDecoder="msmpeg4v1"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=msmpeg4v2 --enable-decoder=msmpeg4v3"
+fi
+
+if [[ $decode == "rv" ]]; then
+  realDecoder="rv10"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=rv20 --enable-decoder=rv30 --enable-decoder=rv40"
+fi
+
+if [[ $decode == "ra" ]]; then
+  realDecoder="cook"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=sipr --enable-decoder=ralf"
+fi
+
+if [[ $decode == "wmv" ]]; then
+  realDecoder="wmv1"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=wmv2 --enable-decoder=wmv3"
+fi
+
+if [[ $decode == "wma" ]]; then
+  realDecoder="wmav1"
+  EXTRACOMPONENTS="$EXTRACOMPONENTS --enable-decoder=wmav2 --enable-decoder=wmapro --enable-decoder=wmavoice --enable-decoder=wmalossless"
+fi
+
+if [[ $decode == "pcm" ]]; then
+  realDecoder="pcm_alaw"
+  EXTRACOMPONENTS="
+    $EXTRACOMPONENTS
+    --enable-decoder=pcm_alaw_at
+    --enable-decoder=pcm_bluray
+    --enable-decoder=pcm_dvd
+    --enable-decoder=pcm_f16le
+    --enable-decoder=pcm_f24le
+    --enable-decoder=pcm_f32be
+    --enable-decoder=pcm_f32le
+    --enable-decoder=pcm_f64be
+    --enable-decoder=pcm_f64le
+    --enable-decoder=pcm_lxf
+    --enable-decoder=pcm_mulaw
+    --enable-decoder=pcm_mulaw_at
+    --enable-decoder=pcm_s16be
+    --enable-decoder=pcm_s16be_planar
+    --enable-decoder=pcm_s16le
+    --enable-decoder=pcm_s16le_planar
+    --enable-decoder=pcm_s24be
+    --enable-decoder=pcm_s24daud
+    --enable-decoder=pcm_s24le
+    --enable-decoder=pcm_s24le_planar
+    --enable-decoder=pcm_s32be
+    --enable-decoder=pcm_s32le
+    --enable-decoder=pcm_s32le_planar
+    --enable-decoder=pcm_s64be
+    --enable-decoder=pcm_s64le
+    --enable-decoder=pcm_s8
+    --enable-decoder=pcm_s8_planar
+    --enable-decoder=pcm_sga
+    --enable-decoder=pcm_u16be
+    --enable-decoder=pcm_u16le
+    --enable-decoder=pcm_u24be
+    --enable-decoder=pcm_u24le
+    --enable-decoder=pcm_u32be
+    --enable-decoder=pcm_u32le
+    --enable-decoder=pcm_u8
+    --enable-decoder=pcm_vidc
+    "
+fi
+
+if [[ $decode == "adpcm" ]]; then
+  realDecoder="adpcm_4xm"
+  EXTRACOMPONENTS="
+    $EXTRACOMPONENTS
+    --enable-decoder=adpcm_adx
+    --enable-decoder=adpcm_afc
+    --enable-decoder=adpcm_agm
+    --enable-decoder=adpcm_aica
+    --enable-decoder=adpcm_argo
+    --enable-decoder=adpcm_ct
+    --enable-decoder=adpcm_dtk
+    --enable-decoder=adpcm_ea
+    --enable-decoder=adpcm_ea_maxis_xa
+    --enable-decoder=adpcm_ea_r1
+    --enable-decoder=adpcm_ea_r2
+    --enable-decoder=adpcm_ea_r3
+    --enable-decoder=adpcm_ea_xas
+    --enable-decoder=adpcm_g722
+    --enable-decoder=adpcm_g726
+    --enable-decoder=adpcm_g726le
+    --enable-decoder=adpcm_ima_acorn
+    --enable-decoder=adpcm_ima_alp
+    --enable-decoder=adpcm_ima_amv
+    --enable-decoder=adpcm_ima_apc
+    --enable-decoder=adpcm_ima_apm
+    --enable-decoder=adpcm_ima_cunning
+    --enable-decoder=adpcm_ima_dat4
+    --enable-decoder=adpcm_ima_dk3
+    --enable-decoder=adpcm_ima_dk4
+    --enable-decoder=adpcm_ima_ea_eacs
+    --enable-decoder=adpcm_ima_ea_sead
+    --enable-decoder=adpcm_ima_iss
+    --enable-decoder=adpcm_ima_moflex
+    --enable-decoder=adpcm_ima_mtf
+    --enable-decoder=adpcm_ima_oki
+    --enable-decoder=adpcm_ima_qt
+    --enable-decoder=adpcm_ima_qt_at
+    --enable-decoder=adpcm_ima_rad
+    --enable-decoder=adpcm_ima_smjpeg
+    --enable-decoder=adpcm_ima_ssi
+    --enable-decoder=adpcm_ima_wav
+    --enable-decoder=adpcm_ima_ws
+    --enable-decoder=adpcm_ms
+    --enable-decoder=adpcm_mtaf
+    --enable-decoder=adpcm_psx
+    --enable-decoder=adpcm_sbpro_2
+    --enable-decoder=adpcm_sbpro_3
+    --enable-decoder=adpcm_sbpro_4
+    --enable-decoder=adpcm_swf
+    --enable-decoder=adpcm_thp
+    --enable-decoder=adpcm_thp_le
+    --enable-decoder=adpcm_vima
+    --enable-decoder=adpcm_xa
+    --enable-decoder=adpcm_xmd
+    --enable-decoder=adpcm_yamaha
+    --enable-decoder=adpcm_zork
+    "
+fi
+
 emmake make clean
 
 emconfigure ./configure \
